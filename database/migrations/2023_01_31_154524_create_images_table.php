6e9b0cd5f18098ea4c4632d5d784ba84905609e9
Pavel Kostitsyn <paulkostitsyn@gmail.com>
2023-01-31 19:51:54 +0400

    Article, image migrations

---
+++ database/migrations/2023_01_31_154524_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('article_id');
+            $table->timestamps();
+
+            $table->unique(['name', 'article_id']);
+            $table->foreign('article_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+};
