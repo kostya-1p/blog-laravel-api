01b48025f539c4a823fc7c6e4774364ac43d93e5
Pavel Kostitsyn <paulkostitsyn@gmail.com>
2023-02-24 08:45:41 +0400

    feat(Article Images CRUD): Add image to article

--- app/Http/Controllers/V1/ArticleImageController.php
+++ app/Http/Controllers/V1/ArticleImageController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreImageRequest;
 use App\Http\Resources\ImageResource;
 use App\Models\Article;
 use App\Repositories\Interfaces\ImageRepositoryInterface;
+use App\Services\ImageService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ArticleImageController extends Controller
 {
-    public function __construct(private ImageRepositoryInterface $imageRepository)
+    public function __construct(private ImageRepositoryInterface $imageRepository, private ImageService $imageService)
     {
     }
 
@@ -23,5 +25,18 @@ class ArticleImageController extends Controller
 
         $images = $this->imageRepository->getByArticle($article);
         return ImageResource::collection($images);
+    }
+
+    public function store(Article $article, StoreImageRequest $request): ImageResource
+    {
+        if ($article->author_id !== $request->user()->id) {
+            return abort(403, 'Unauthorized action.');
+        }
+
+        $imageName = $this->imageService->generateImageName($request->image);
+        $this->imageService->saveImage($article, $request->image, $imageName);
+        $image = $this->imageService->make($imageName, $article->id);
+
+        return new ImageResource($image);
     }
 }
